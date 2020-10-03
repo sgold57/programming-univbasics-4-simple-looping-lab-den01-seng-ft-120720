efcea98943a5163356b62cc003db8efa4c1faa57
Sam Gold <sgold5757@gmail.com>
2020-10-03 20:04:20 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -16,5 +16,10 @@ def output_array(array)
   end
 end
 
-def return_string_array
+def return_string_array(array)
+  counter = 0
+  while counter < array.length do
+    array[counter] = array[counter].to_s
+    counter += 1
+  end
 end
